b1f12fa198059c5e372d284bf5cb1c85179e7142
KorchinskiyIlya <korchinskiy.ilya@gmail.com>
2019-11-24 17:51:26 +0300

    example components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const element = <h1 className="react-hello">
-    Hello! React JSX works!
-</h1>;
+const messages = ['Hello, my friend', 'How are you doing?'];
 
+const Message = (props) => <div>{props.text}</div>;
+
+const MessageList = (props) => {
+    return props.messages.map(message => <Message text={message}/>)
+};
 
 ReactDom.render(
-    element,
+    <MessageList messages={messages}/>,
     document.getElementById('root')
 );
 
